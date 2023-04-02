626572e58fd6d137441c83c57282729cf6584ca2
rirefat <rirefat.official@gmail.com>
2023-04-03 05:24:39 +0600

    done about section with responsive

--- src/Folder Structure/Components/About/About.js
+++ src/Folder Structure/Components/About/About.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './About.css';
 import SectionHeader from '../../Shared/SectionHeader/SectionHeader';
+import mechanic from '../../../assets/images/about_us/person.jpg';
+import parts from '../../../assets/images/about_us/parts.jpg';
 
 const About = () => {
     return (
         <div className='about'>
             <div className="about-left">
-                left section
+                <div className="big-screen">
+                    <img className='mechanic-img' src={mechanic} alt="Car Mechanic Img" />
+                    <img className='parts-img' src={parts} alt="Car Parts" />
+                </div>
+                {/* <div className="small-screen">
+                    <img className='mechanic-img' src={mechanic} alt="Car Mechanic Img" />
+                    <img className='parts-img' src={parts} alt="Car Parts" />
+                </div> */}
             </div>
+
             <div className="about-right text-left">
                 <div className="section-title">
                     <SectionHeader
@@ -19,7 +29,7 @@ const About = () => {
                 <p className='description'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
                 <p className='description'>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
 
-                <button className="btn shadow-md">Get More Info</button>
+                <button className="btn shadow-md my-4">Get More Info</button>
             </div>
         </div>
     );
