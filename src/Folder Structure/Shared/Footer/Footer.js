ec486580ee978a72cf31d044b47e944058dee5df
rirefat <rirefat.official@gmail.com>
2023-04-03 02:51:31 +0600

    modify footer

--- src/Folder Structure/Shared/Footer/Footer.js
+++ src/Folder Structure/Shared/Footer/Footer.js
@@ -9,24 +9,24 @@ const Footer = () => {
             <footer className="footer p-10 text-base-content">
                 <div>
                     <Link to={'/'}><img src={logoFooter} alt="Logo" /></Link>
-                    <p className='text-base text-gray-600'>Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial .</p>
+                    <p className='text-lg text-slate-400 mt-6'>Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial .</p>
                 </div>
                 <div>
-                    <span className="footer_title">About</span>
-                    <Link to={'/home'} className="link link-hover">Home</Link>
-                    <Link to={'/service'} className="link link-hover">Service</Link>
-                    <Link to={'/contact'} className="link link-hover">Contact</Link>
+                    <span className="footer_title text-lg font-semibold text-lg">About</span>
+                    <Link to={'/home'} className="link link-hover text-base">Home</Link>
+                    <Link to={'/service'} className="link link-hover text-base">Service</Link>
+                    <Link to={'/contact'} className="link link-hover text-base">Contact</Link>
                 </div>
                 <div>
-                    <span className="footer_title">Company</span>
-                    <Link to={'/why-car-doctor'} className="link link-hover">Why Car Doctor</Link>
-                    <Link to={'/about'} className="link link-hover">About</Link>
+                    <span className="footer_title text-lg font-semibold text-lg">Company</span>
+                    <Link to={'/why-car-doctor'} className="link link-hover text-base">Why Car Doctor</Link>
+                    <Link to={'/about'} className="link link-hover text-base">About</Link>
                 </div>
                 <div>
-                    <span className="footer_title">Support</span>
-                    <Link to={'/Support'} className="link link-hover">Support Center</Link>
-                    <Link to={'/feedback'} className="link link-hover">Feedback</Link>
-                    <Link to={'/accessability'} className="link link-hover">Accessability</Link>
+                    <span className="footer_title text-lg font-semibold text-lg">Support</span>
+                    <Link to={'/Support'} className="link link-hover text-base">Support Center</Link>
+                    <Link to={'/feedback'} className="link link-hover text-base">Feedback</Link>
+                    <Link to={'/accessability'} className="link link-hover text-base">Accessability</Link>
                 </div>
             </footer>
         </div>
