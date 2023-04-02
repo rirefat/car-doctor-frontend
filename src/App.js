a3727300577837a937125cee54f1a9140be9baec
rirefat <rirefat.official@gmail.com>
2023-04-03 00:38:01 +0600

    prepare basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './Components/Routes/Routes';
 
 const App = () => {
+  
   return (
     <div className='app'>
+      <RouterProvider router={router} />
     </div>
   );
 };
---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    { path: '/', element: <h1>abc</h1> }
+]);
+
+export default router;
