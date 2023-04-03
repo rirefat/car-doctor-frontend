d900a954648d4d019919aef057cddb0770eecac1
rirefat <rirefat.official@gmail.com>
2023-04-03 06:00:57 +0600

    done contact section

--- src/Folder Structure/Pages/Home/Home.js
+++ src/Folder Structure/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../../Components/About/About';
+import Contact from '../../Components/Contact/Contact';
 
 const Home = () => {
     return (
         <div className='home'>
             <About></About>
+            <Contact></Contact>
         </div>
     );
 };
