62b385788cfe2af27408ffed11c23c5ce89a3362
rirefat <rirefat.official@gmail.com>
2023-04-03 19:52:12 +0600

    add hover effect of core features

--- src/Folder Structure/Components/CoreFeatures/CoreFeatures.js
+++ src/Folder Structure/Components/CoreFeatures/CoreFeatures.js
@@ -3,6 +3,7 @@ import './CoreFeatures.css';
 import SectionHeader from '../../Shared/SectionHeader/SectionHeader';
 
 import team from '../../../assets/icons/team.svg';
+import time from '../../../assets/icons/time.svg';
 import support from '../../../assets/icons/support.svg';
 import wrench from '../../../assets/icons/Wrench.svg';
 import guranty from '../../../assets/icons/check.svg';
@@ -11,7 +12,7 @@ import delivery from '../../../assets/icons/deliveryt.svg';
 const CoreFeatures = () => {
     const coreFeatures = [
         {id: 1, title: "Expert Team", icon:team},
-        {id: 2, title: "Timely Delivery", icon:team},
+        {id: 2, title: "Timely Delivery", icon:time},
         {id: 3, title: "24/7 Support", icon:support},
         {id: 4, title: "Best Equipments", icon:wrench},
         {id: 5, title: "100% Guranty", icon:guranty},
@@ -25,7 +26,7 @@ const CoreFeatures = () => {
                 description={"the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."}
                 center={true}
             ></SectionHeader>
-            <div className="features-container my-15">
+            <div className="features-container">
                 {
                     coreFeatures.map(feature=><div key={feature.id} className='feature'>
                         <img src={feature.icon} alt="feature icon" className='mb-5'/>
