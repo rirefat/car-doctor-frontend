2b3ad80eda24391a75eb646a6d0b8b44a7ff6678
rirefat <rirefat.official@gmail.com>
2023-04-03 19:29:40 +0600

    load core features

--- src/Folder Structure/Pages/Home/Home.js
+++ src/Folder Structure/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../../Components/About/About';
 import Contact from '../../Components/Contact/Contact';
+import CoreFeatures from '../../Components/CoreFeatures/CoreFeatures';
 
 const Home = () => {
     return (
         <div className='home'>
             <About></About>
             <Contact></Contact>
+            <CoreFeatures></CoreFeatures>
         </div>
     );
 };
