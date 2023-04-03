31a8d429bba1b66963a5e724f981775c61d071bd
rirefat <rirefat.official@gmail.com>
2023-04-03 06:11:29 +0600

    done contact section

--- src/Folder Structure/Components/Contact/Contact.js
+++ src/Folder Structure/Components/Contact/Contact.js
@@ -6,21 +6,23 @@ import location from '../../../assets/icons/location.png';
 
 const Contact = () => {
     const contactInfo = [
-        {id: 1, title: "we're open monday-friday", icon:schedule, desc: "7:00 am - 9:00 pm"},
-        {id: 2, title: "Have a question?", icon:call, desc: "+2546 251 2658"},
-        {id: 3, title: "Need a repair? our address", icon:location, desc: "Liza Street, New York"},
+        { id: 1, title: "we're open monday-friday", icon: schedule, desc: "7:00 am - 9:00 pm" },
+        { id: 2, title: "Have a question?", icon: call, desc: "+2546 251 2658" },
+        { id: 3, title: "Need a repair? our address", icon: location, desc: "Liza Street, New York" },
     ]
     return (
-        <div className='contact' id='contact'>
-            {
-                contactInfo.map(single=><div key={single.id} className='contact-container'>
-                    <img src={single.icon} alt="icon" />
-                    <div>
-                        <p className='title'>{single.title}</p>
-                        <p className='desc'>{single.desc}</p>
-                    </div>
-                </div>)
-            }
+        <div className="parent-container">
+            <div className='contact' id='contact'>
+                {
+                    contactInfo.map(single => <div key={single.id} className='contact-container'>
+                        <img src={single.icon} alt="icon" />
+                        <div>
+                            <p className='title'>{single.title}</p>
+                            <p className='desc'>{single.desc}</p>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
