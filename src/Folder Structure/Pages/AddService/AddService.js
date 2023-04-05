2fbf0f6a18719a3870a678923919b2db949625d6
rirefat <rirefat.official@gmail.com>
2023-04-06 02:43:07 +0600

    get input data from input table

--- src/Folder Structure/Pages/AddService/AddService.js
+++ src/Folder Structure/Pages/AddService/AddService.js
@@ -3,6 +3,18 @@ import './AddService.css';
 import TopBanner from '../../Shared/TopBanner/TopBanner';
 
 const AddService = () => {
+    const addService=(event)=>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const description = event.target.description.value;
+        const price = event.target.price.value;
+        const img = event.target.img.value;
+
+        const data ={name, description, price, img};
+        console.log(data);
+        event.target.reset();
+    }
+
     return (
         <div className='add-service-page'>
             <TopBanner
@@ -10,13 +22,13 @@ const AddService = () => {
                 subtitle={"--"}
             ></TopBanner>
             <div className="add-service-form my-16">
-                <form>
+                <form onSubmit={addService}>
                     <div className='add-service-form-top grid grid-cols-3 gap-3.5 mb-3.5'>
-                        <input required type="text" placeholder="Service Name" className=" input w-full " />
-                        <input required type="number" placeholder="Service Price" className=" input w-full " />
-                        <input required type="url" placeholder="Service Image URL" className=" input w-full " />
+                        <input name='name' required type="text" placeholder="Service Name" className=" input w-full " />
+                        <input name='price' required type="number" placeholder="Service Price" className=" input w-full " />
+                        <input name='img' required type="url" placeholder="Service Image URL" className=" input w-full " />
                     </div>
-                    <textarea required placeholder="Service Description" className=" textarea textarea-lg w-full" ></textarea>
+                    <textarea name='description' required placeholder="Service Description" className=" textarea textarea-lg w-full" ></textarea>
                     <button className="btn add-service-form-btn shadow-md w-full mt-6 bg-primary-color">Add Service</button>
                 </form>
             </div>
