8a540985df79f34bf8ed9bef8c04d24106878399
rirefat <rirefat.official@gmail.com>
2023-04-07 04:36:07 +0600

    Add a state for the displayed services in the admin dashboard to update the panel instantly

--- src/Folder Structure/DashboardComponents/AvailableServices/AvailableServices.js
+++ src/Folder Structure/DashboardComponents/AvailableServices/AvailableServices.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Table from '../Table/Table';
 import { IoMdAdd } from 'react-icons/io';
 import { ToastContainer, toast } from 'react-toastify';
 
 const AvailableServices = () => {
-    const availableServices = useLoaderData();
     const notify = () => toast("Deleted Successfully!");
+    const availableServices = useLoaderData();
+    const [displayedServices, setDisplayedServices]=useState(availableServices);    
 
     const handleServiceDelete = (id) => {
-        console.log(id, "clicked")
         fetch(`http://localhost:5000/services/${id}`, {
             method: "DELETE",
             headers: {
@@ -18,6 +18,8 @@ const AvailableServices = () => {
         })
             .then(res => res.json())
             .then(data => {
+                const newServices = availableServices.filter(service=> service._id !== id);
+                setDisplayedServices(newServices);
                 notify();
             })
     }
@@ -27,12 +29,12 @@ const AvailableServices = () => {
             <h1 className='text-center text-2xl font-semibold mb-8'><span className='text-primary-color'>Services</span> At A Glance</h1>
 
             <div className="services-summery flex justify-between items-center p-4 border-b-2 border-stone-300 mb-4">
-                <p className='text-xl font-medium'>All Services: {availableServices.length}</p>
+                <p className='text-xl font-medium'>All Services: {displayedServices.length}</p>
                 <Link to={'/add-service'}><button className="shadow-xl btn btn-square btn-outline text-2xl hover:text-white"><IoMdAdd></IoMdAdd></button></Link>
             </div>
             <div className="services-container">
                 {
-                    availableServices.map(singleService => <Table key={singleService._id} singleService={singleService} handleServiceDelete={handleServiceDelete}></Table>)
+                    displayedServices.map(singleService => <Table key={singleService._id} singleService={singleService} handleServiceDelete={handleServiceDelete}></Table>)
                 }
             </div>
             <ToastContainer />
