a0cb09b4557c01585b6f80963454c52168261b8c
rirefat <rirefat.official@gmail.com>
2023-04-06 06:44:19 +0600

    done navigation panel of admin panel

--- src/Folder Structure/Pages/AdminDashboard/AdminDashboard.js
+++ src/Folder Structure/Pages/AdminDashboard/AdminDashboard.js
@@ -1,6 +1,12 @@
 import './AdminDashboard.css'
 import TopBanner from '../../Shared/TopBanner/TopBanner';
 import { Link, Outlet } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
+
+import { BsFillBoxSeamFill, BsWrenchAdjustable } from 'react-icons/bs';
+import { RiTeamFill } from 'react-icons/ri';
+// import { HiWrenchScrewdriver } from 'react-icons/hi';
+import { GrOverview } from 'react-icons/gr';
 
 const AdminDashboard = () => {    
 
@@ -13,11 +19,13 @@ const AdminDashboard = () => {
                 ></TopBanner>
                 <div className="dashboard-main-body">
                     <div className="navigation-panel">
-                        <Link to={'/home'}>Home</Link>
-                        <Link to={'/admin-dashboard'}>Overview</Link>
-                        <Link to={'/admin-dashboard/services'}>Available Services</Link>
-                        <Link to={'/admin-dashboard/products'}>Available Products</Link>
-                        <Link to={'/admin-dashboard/team'}>Available Team Members</Link>
+                        <Link to={'/home'} className='logo'>
+                            <img src={logo} alt="" />
+                        </Link>
+                        <Link to={'/admin-dashboard'}><span className='icon'><GrOverview></GrOverview></span>Overview</Link>
+                        <Link to={'/admin-dashboard/services'}><span className='icon'><BsWrenchAdjustable></BsWrenchAdjustable></span>Available Services</Link>
+                        <Link to={'/admin-dashboard/products'}><span className='icon'><BsFillBoxSeamFill></BsFillBoxSeamFill></span> Available Products</Link>
+                        <Link to={'/admin-dashboard/team'}><span className='icon'><RiTeamFill></RiTeamFill></span>Available Team Members</Link>
                     </div>
                     <div className="rights-panel">
                         <Outlet></Outlet>
