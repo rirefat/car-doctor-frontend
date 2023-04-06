cd8628917b6ef046e49a57a6233e6a5ea48186e7
rirefat <rirefat.official@gmail.com>
2023-04-07 05:20:34 +0600

    done delete operation for removing products

--- src/Folder Structure/DashboardComponents/AvailableProducts/AvailableProducts.js
+++ src/Folder Structure/DashboardComponents/AvailableProducts/AvailableProducts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdAdd } from 'react-icons/io';
 import { AiFillDelete } from 'react-icons/ai';
 import { RxUpdate } from 'react-icons/rx';
@@ -6,18 +6,43 @@ import { Link, useLoaderData } from 'react-router-dom';
 
 const AvailableProducts = () => {
     const availableProducts = useLoaderData();
+    const [displayedProducts, setDisplayedProducts]=useState(availableProducts);
+
+    const handleDeleteProduct=(id)=>{
+        const agree = window.confirm("Are you sure to delete?");
+        if(agree){
+            fetch(`http://localhost:5000/products/${id}`,{
+                method: "DELETE",
+                headers: {
+                    'content-type': 'applicaion/json',
+                }
+            })
+                .then(res=>res.json())
+                .then(data=>{
+                    const newSet = availableProducts.filter(product => product._id !== id);
+                    setDisplayedProducts(newSet)
+                    alert("deleted")
+                })
+        }
+        else{
+            window.confirm("Request denied.");
+        }
+    }
+
+    const handleUpdateProduct=(id)=>{}
+
     return (
         <div className='dashboard-products p-8'>
             <h1 className='text-center text-2xl font-semibold mb-8'><span className='text-primary-color'>Products</span> At A Glance</h1>
 
             <div className="products-summery flex justify-between items-center p-4 border-b-2 border-stone-300 mb-4">
-                <p className='text-xl font-medium'>All Products: {availableProducts.length}</p>
+                <p className='text-xl font-medium'>All Products: {displayedProducts.length}</p>
                 <Link to={'/add-product'}><button className="shadow-xl btn btn-square btn-outline text-2xl hover:text-white"><IoMdAdd></IoMdAdd></button></Link>
             </div>
 
             <div className="products-container">
                 {
-                    availableProducts.map(singleProduct => <div key={singleProduct._id}>
+                    displayedProducts.map(singleProduct => <div key={singleProduct._id}>
                         <div className="overflow-x-auto w-full mb-2">
                             <div className="overflow-x-auto w-full">
                                 <table className="table w-full">
@@ -42,8 +67,8 @@ const AvailableProducts = () => {
                                                 {/* <Link to={`/service-details/${_id}`}><button className="btn btn-ghost btn-xs">Details</button></Link> */}
                                             </td>
                                             <th className='flex justify-end'>
-                                                <button className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><RxUpdate></RxUpdate></button>
-                                                <button className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><AiFillDelete></AiFillDelete></button>
+                                                <button onClick={()=>handleUpdateProduct(singleProduct._id)} className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><RxUpdate></RxUpdate></button>
+                                                <button onClick={()=>handleDeleteProduct(singleProduct._id)} className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><AiFillDelete></AiFillDelete></button>
                                             </th>
                                         </tr>
                                     </tbody>
