7ce50ef4cbeed1eaf41f2ad62934b15df83a74e7
rirefat <rirefat.official@gmail.com>
2023-04-09 03:35:16 +0600

    add update functionality for products

--- src/Folder Structure/DashboardComponents/UpdateProduct/UpdateProduct.js
+++ src/Folder Structure/DashboardComponents/UpdateProduct/UpdateProduct.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
-const UpdateProduct = () => {
+const UpdateProduct = (event) => {
+    const notify = () => toast("Successfully Updated!");
     const product = useLoaderData();
-    console.log(product);
+
+    const handleUpdateProduct = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const description = event.target.description.value;
+        const price = event.target.price.value;
+        const img = event.target.img.value;
+
+        const data = { name, description, price, img };
+        const agree = window.confirm("Are you sure to update?");
+
+        if (agree) {
+            fetch(`http://localhost:5000/products/${product._id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            })
+                .then(res => res.json())
+                .then(data => {
+                    notify();
+                })
+        }
+        else {
+            window.alert("Request is rejected.")
+        }
+    }
+
     return (
         <div className='update-product p-8'>
             <h1 className='text-center text-2xl font-semibold mb-8'>Update Product: <span className='text-primary-color'>{product.name}</span></h1>
-            <form action="">
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" /> 
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-
-            <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
-
-            <button className="btn btn-outline">Button</button>
+            <form onSubmit={handleUpdateProduct}>
+                <div className='add-product-form-top grid grid-cols-3 gap-3.5 mb-3.5'>
+                    <input defaultValue={product.name} name='name' type="text" placeholder="Product Name" className=" input input-bordered w-full " />
+                    <input defaultValue={product.price} name='price' type="number" placeholder="Product Price" className=" input input-bordered w-full " />
+                    <input defaultValue={product.img} name='img' type="url" placeholder="Product Image(314*314 px) URL" className=" input input-bordered w-full " />
+                </div>
+                <textarea defaultValue={product.description} name='description' required placeholder="Product Description" className="input-bordered textarea textarea-lg w-full" ></textarea>
+                <button className="btn add-product-form-btn shadow-md w-full mt-6 bg-primary-color border-transparent">Update product</button>
             </form>
+            <ToastContainer/>
         </div>
     );
 };
--- src/Folder Structure/DashboardComponents/UpdateService/UpdateService.js
+++ src/Folder Structure/DashboardComponents/UpdateService/UpdateService.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 const UpdateService = () => {
     const notify = () => toast("Successfully Updated!");
