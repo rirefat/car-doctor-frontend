56d2d71c4ce5a8951448bc5e1e93a7cc26e106c0
rirefat <rirefat.official@gmail.com>
2023-04-09 21:25:36 +0600

    added teams information to the admin panel

--- src/Folder Structure/DashboardComponents/Manpower/Manpower.js
+++ src/Folder Structure/DashboardComponents/Manpower/Manpower.js
@@ -1,6 +1,12 @@
 import React from 'react';
+
 import { IoMdAdd } from 'react-icons/io';
-import { useLoaderData } from 'react-router-dom';
+import { AiFillDelete } from 'react-icons/ai';
+import { RxUpdate } from 'react-icons/rx';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaFacebook, FaTwitter } from 'react-icons/fa';
+
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Manpower = () => {
     const availableManpower = useLoaderData()
@@ -13,10 +19,45 @@ const Manpower = () => {
             </div>
             <div className="manpower-container">
                 {
-                    // summeryBoxex.map(summery => <div key={summery.id} className='summery-box shadow-xl'>
-                    //     <img src={summery.icon} alt="Icon" className='mb-4' />
-                    //     <p className='font-medium'>{summery.name}: {summery.info?summery.info:"0"}</p>
-                    // </div>)
+                    availableManpower.map(person => <div className='manpower-container' key={person._id}>
+                        <div className="overflow-x-auto w-full mb-2">
+                            <div className="overflow-x-auto w-full">
+                                <table className="table w-full">
+                                    <tbody>
+                                        <tr>
+                                            <td className='w-1/2'>
+                                                <div className="flex items-center space-x-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle w-12 h-12">
+                                                            <img src={person.picture} alt="Avatar Tailwind CSS Component" />
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div className="font-bold">{person.name}</div>
+                                                        <div className="text-sm opacity-50">{person.title}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className='w-2'>
+                                                <p className='text-2xl font-semibold flex gap-4'>
+                                                    <Link className='hover:text-blue-500' to={person.social_links.facebook}><FaFacebook></FaFacebook></Link>
+                                                    <Link className='hover:text-blue-700' to={person.social_links.twitter}><FaTwitter></FaTwitter></Link>
+                                                    <Link className='hover:text-blue-950' to={person.social_links.linkedin}><BsLinkedin></BsLinkedin></Link>
+                                                </p>
+                                            </td>
+                                            <td>
+                                                {/* <Link to={`/service-details/${_id}`}><button className="btn btn-ghost btn-xs">Details</button></Link> */}
+                                            </td>
+                                            <th className='flex justify-end'>
+                                                <button className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><RxUpdate></RxUpdate></button>
+                                                <button className="btn btn-circle btn-outline text-lg mx-3 hover:text-white"><AiFillDelete></AiFillDelete></button>
+                                            </th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>)
                 }
             </div>
         </div>
